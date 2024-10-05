71f4a055024e97c8edad5d46956bf888dd64dd8b
ShashwatXD <legendshashwat.gkp@gmail.com>
2024-10-05 20:23:23 +0530

    latest

---
+++ lib/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            const Text('Login Screen'),
+           //ui comms from here
+          ],
+        ),
+      ),
+    );
+  }
+}
